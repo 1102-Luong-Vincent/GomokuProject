40cf2ca03fe451103fae4b0f0983df83c9b2ca8c
Vincent Luong <89359930+1102-Luong-Vincent@users.noreply.github.com>
2025-10-24 19:14:47 -0700

    added a* and PF script

---
+++ Assets/Script/Algorithm/AStarAlgorithm.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public static class AStarAlgorithm
+{
+    //public static List<(int, int)> AStarFindPath(GomoKuType[,] board, (int, int) endPoint)
+    //{
+    //    List<(int, int)> Path = new List<(int, int)>();
+
+
+    //    int x, y;
+    //    if (board[x, y] == GomoKuType.None)
+    //    {
+
+    //    }
+
+    //    return Path;
+    //}
+}
